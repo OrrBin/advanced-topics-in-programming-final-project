66f12c4b5c4ef0ab33bc812d864e478eb87b21c4
Orr Benyamini <orr.bob@hotmail.com>
2020-07-07 18:39:45 +0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/final_project.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/final_project.dir/main.cpp.obj"
+  "final_project.exe"
+  "final_project.exe.manifest"
+  "final_project.pdb"
+  "libfinal_project.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/final_project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
